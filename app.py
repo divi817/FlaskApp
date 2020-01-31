752d32342b844deba04e4a519fc831798d3faa6e
divi817 <58705833+divi817@users.noreply.github.com>
2020-01-31 17:41:42 +0100

    update 1.0

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases/tasks.db'
 db = SQLAlchemy(app)
 
 @app.route('/')
 
 def home():
-    return 'Hello World'
+    return render_template("index.html")
 
 if __name__== '__main__':
     app.run(debug=True)
